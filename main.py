762e2d5d996d92e2b4a7ab369ce0e4fae5262401
pengzhile <pengzhile@gmail.com>
2023-07-24 15:07:32 +0800

    switch to remote login
    Signed-off-by: pengzhile <pengzhile@gmail.com>

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from pandora_cloud.server import ChatBot
 _port = getenv('PORT')
 _proxy = getenv('PANDORA_PROXY')
 _debug = getenv('PANDORA_DEBUG', 'false').lower() == 'true'
-_sentry = getenv('PANDORA_SENTRY', 'false').lower() == 'true'
+_local = getenv('PANDORA_LOGIN_LOCAL', 'false').lower() == 'true'
 _listen = getenv('PANDORA_SERVER_LISTEN', 'true').lower() == 'true'
 _server = getenv('PANDORA_SERVER', '0.0.0.0:{}'.format(_port) if _port else '0.0.0.0')
 
-app = ChatBot(proxy=_proxy, debug=_debug, sentry=_sentry, login_local=True).run(_server, listen=_listen)
+app = ChatBot(proxy=_proxy, debug=_debug, login_local=_local).run(_server, listen=_listen)
